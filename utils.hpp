673562e1372e6ea6a74ea5181164e5d83d4cf852
Txema Otero <txedav@gmail.com>
2023-12-07 22:43:37 +0100

    Added an utils file

---
+++ utils.hpp
@@ -0,0 +1,32 @@
+/*
+ * This file defines some utility functions that are useful for multiple days
+ */
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> split(std::string s, const std::string& delimiter)
+{
+    std::vector<std::string> tokens;
+    std::string token;
+    size_t pos = 0;
+    while ((pos = s.find(delimiter)) != std::string::npos)
+    {
+        token = s.substr(0, pos);
+        tokens.push_back(token);
+        s.erase(0, pos + delimiter.length());
+    }
+    tokens.push_back(s);
+    return tokens;
+}
+
+std::string strip(std::string s)
+{
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+        return !std::isspace(ch);
+    }));
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+    return s;
+}
