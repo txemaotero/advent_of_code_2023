4b9227a0244c37b6ab05cf26f757152740ccd358
Txema Otero <txedav@gmail.com>
2023-12-25 10:06:52 +0100

    Day 13 finished part 1

--- day_13/main.cpp
+++ day_13/main.cpp
@@ -1,6 +1,6 @@
+#include <algorithm>
 #include <fstream>
 #include <optional>
-#include <ranges>
 #include <string>
 #include <iostream>
 #include "../utils.hpp"
@@ -23,45 +23,81 @@ public:
 
     void addRow(const std::string& row) {
         std::vector<bool> newRow;
-        for (const auto& c : row) {
+        bool first = patternTransposed.empty();
+        for (int i = 0; const auto& c : row) {
             newRow.push_back(c == '#');
+            if (first) {
+                patternTransposed.push_back({});
+            }
+            patternTransposed[i++].push_back(c == '#');
         }
         pattern.push_back(newRow);
     };
 
     void clear() {
         pattern.clear();
+        patternTransposed.clear();
     }
 
     uint64 calcPart1() {
         uint64 result = 0;
         auto reflexionColumn = calcReflexionColumn();
         if (reflexionColumn)
+        {
             result += (*reflexionColumn) + 1;
+        }
         auto reflexionRow = calcReflexionRow();
         if (reflexionRow)
+        {
             result += 100*((*reflexionRow) + 1);
+        }
         return result;
     }
 
     std::optional<size_t> calcReflexionColumn()
     {
-        return {};
+        return calcReflexionIndex(patternTransposed);
     }
 
     std::optional<size_t> calcReflexionRow()
     {
-        return {};
+        return calcReflexionIndex(pattern);
     }
 
 private:
     std::vector<std::vector<bool>> pattern;
+    std::vector<std::vector<bool>> patternTransposed;
+
+    std::optional<size_t> calcReflexionIndex(const std::vector<std::vector<bool>>& matrix)
+    {
+        for (size_t row = 0; row < matrix.size(); ++row) {
+            if (checkReflexion(matrix, row))
+            {
+                return row;
+            }
+        }
+        return {};
+    }
+
+    bool checkReflexion(const std::vector<std::vector<bool>>& matrix, size_t row)
+    {
+        size_t nReflexions = std::min(row+1, matrix.size() - row - 1);
+        if (nReflexions == 0) {
+            return false;
+        }
+        for (size_t refIndex = 0; refIndex < nReflexions; ++refIndex) {
+            if (!std::ranges::equal(matrix[row - refIndex], matrix[row + refIndex + 1], std::ranges::equal_to{})) {
+                return false;
+            }
+        }
+        return true;
+    }
 };
 
 
 int main() {
-    // std::ifstream file("input.txt");
-    std::ifstream file("example.txt");
+    std::ifstream file("input.txt");
+    // std::ifstream file("example.txt");
     if (!file) {
         std::cerr << "Error opening file\n";
         return 1;
@@ -70,9 +106,11 @@ int main() {
     std::string line;
     Pattern pattern;
     uint64 part1 = 0;
+    int i = 1;
     while (std::getline(file, line)) {
         if (line.empty()) {
-            part1 += pattern.calcPart1();
+            auto aux = pattern.calcPart1();
+            part1 += aux;
             pattern.clear();
         }
         else
@@ -80,7 +118,8 @@ int main() {
             pattern.addRow(line);
         }
     }
-    part1 += pattern.calcPart1();
+    auto aux = pattern.calcPart1();
+    part1 += aux;
     std::cout << "Part 1: " << part1 << "\n";
 
     return 0;
