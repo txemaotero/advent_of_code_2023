54598e5519e6f1292c1629646451c923f9004f2b
Txema Otero <txedav@gmail.com>
2023-12-25 14:36:30 +0100

    Finished day 14

--- day_14/main.cpp
+++ day_14/main.cpp
@@ -1,6 +1,5 @@
-#include <algorithm>
 #include <fstream>
-#include <ranges>
+#include <list>
 #include <string>
 #include <iostream>
 #include "../utils.hpp"
@@ -45,6 +44,61 @@ public:
         }
     }
 
+    void tiltSouth() {
+        for (uint32 i = nRows - 1; i < nRows; --i) {
+            for (uint32 j = 0; j < nCols; ++j) {
+                if (maze[i][j] != 2) {
+                    continue;
+                }
+                uint32 destRow = i;
+                while (destRow < nRows - 1 && maze[destRow + 1][j] == 0) {
+                    ++destRow;
+                }
+                maze[i][j] = 0;
+                maze[destRow][j] = 2;
+            }
+        }
+    }
+
+    void tiltWest() {
+        for (uint32 i = 0; i < nRows; ++i) {
+            for (uint32 j = 0; j < nCols; ++j) {
+                if (maze[i][j] != 2) {
+                    continue;
+                }
+                uint32 destCol = j;
+                while (destCol > 0 && maze[i][destCol - 1] == 0) {
+                    --destCol;
+                }
+                maze[i][j] = 0;
+                maze[i][destCol] = 2;
+            }
+        }
+    }
+
+    void tiltEast () {
+        for (uint32 i = 0; i < nRows; ++i) {
+            for (uint32 j = nCols - 1; j < nCols; --j) {
+                if (maze[i][j] != 2) {
+                    continue;
+                }
+                uint32 destCol = j;
+                while (destCol < nCols - 1 && maze[i][destCol + 1] == 0) {
+                    ++destCol;
+                }
+                maze[i][j] = 0;
+                maze[i][destCol] = 2;
+            }
+        }
+    }
+
+    void cycle() {
+        tiltNorth();
+        tiltWest();
+        tiltSouth();
+        tiltEast();
+    }
+
     uint32 calcLoad() {
         uint32 load = 0;
         for (uint32 i = 0; i < nRows; ++i) {
@@ -55,6 +109,14 @@ public:
             }
         }
         return load;
+    }
+
+    int operator[](uint32 i, uint32 j) const {
+        return maze[i][j];
+    }
+
+    bool operator==(const Maze& other) const {
+        return maze == other.maze;
     }
 
 private:
@@ -75,8 +137,33 @@ int main() {
     while (std::getline(file, line)) {
         maze.addRow(line);
     }
+    Maze maze2 = maze;
     maze.tiltNorth();
     std::cout << "Part 1: " << maze.calcLoad() << "\n";
+
+    std::list<Maze> mazeCache;
+    uint64 i;
+    for (i = 0; i < 1e9; ++i) {
+        maze2.cycle();
+        if (std::find(mazeCache.begin(), mazeCache.end(), maze2) != mazeCache.end()) {
+            break;
+        }
+        if (mazeCache.size() < 100) {
+            mazeCache.push_back(maze2);
+        } else {
+            mazeCache.pop_front();
+            mazeCache.push_back(maze2);
+        }
+    }
+    uint64 cycleEnd = i;
+    uint64 cycleStart = i - (mazeCache.size() - std::distance(mazeCache.begin(), std::find(mazeCache.begin(), mazeCache.end(), maze2)));
+    uint64 cycleLength = cycleEnd - cycleStart;
+    uint64 remaining = static_cast<uint64>(1e9 - cycleStart) % cycleLength;
+    for (i = 0; i < remaining - 1; ++i) {
+        maze2.cycle();
+    }
+
+    std::cout << "Part 2: " << maze2.calcLoad() << "\n";
 
     return 0;
 }
